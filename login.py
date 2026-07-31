267329842a8a4010ccbfab6df063b5f96261ba58
sajjad <sajjadazizi803@gmail.com>
2026-07-31 19:15:15 +0330

    qwe

--- login.py
+++ login.py
@@ -4,5 +4,12 @@ from telethon.sessions import StringSession
 api_id = 38174523
 api_hash = "07b04824d0e1185a59b919ba672ec679"
 
-with TelegramClient(StringSession(), api_id, api_hash) as client:
-    print(client.session.save())
+client = TelegramClient(StringSession(), api_id, api_hash)
+
+client.start(phone=lambda: input("Phone: "))
+
+print("\n\n========== STRING SESSION ==========\n")
+print(client.session.save())
+print("\n====================================\n")
+
+client.disconnect()
