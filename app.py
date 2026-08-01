a24a8996471373ffd5b017ddf4fde55c8fd29bb2
sajjad <sajjadazizi803@gmail.com>
2026-08-01 10:22:02 +0330

    redcs

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 from bot import create_bot
+
 import database
-import asyncio
-from bot import tg_client
 
 
 def main():
 
-    print("🔄 Connecting Telegram account...")
-
-    asyncio.run(tg_client.start())
-
-    print("✅ Telegram account connected.")
+    print("🔮 PostGozar Started...")
 
     app = create_bot()
-
-    print("🔮 PostGozar Started...")
 
     app.run_polling()
 
